99a2084bb52f2671a31ba56b0eb54748cce42b33
silverman2539 <63432265+silverman2539@users.noreply.github.com>
2021-05-26 12:41:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function Quote1()
+{
+	document.getElementById("quote1").style.display = "none";
+	document.getElementById("quote2").style.display = "";
+}
+
+function Quote2()
+{
+	document.getElementById("quote2").style.display = "none";
+	document.getElementById("quote3").style.display = "";
+}
+
+function Quote3()
+{
+	document.getElementById("quote3").style.display = "none";
+	document.getElementById("quote4").style.display = "";
+}
+
+function Quote4()
+{
+	document.getElementById("quote4").style.display = "none";
+	document.getElementById("quote5").style.display = "";
+}
+
+function Quote5()
+{
+	document.getElementById("quote5").style.display = "none";
+	document.getElementById("quote1").style.display = "";
+}
